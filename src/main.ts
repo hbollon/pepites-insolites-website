76c78dab61e23ca57aadbc9d6cd35b75d8a11d1a
hbollon <hugo.bollon@gmail.com>
2022-02-12 19:24:37 +0100

    feat: import bulma and override some bulma's constants

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-createApp(App).use(router).mount('#app')
+import '@/styles/global.scss'
+import '@/styles/constants.scss'
+
+createApp(App)
+  .use(router)
+  .mount("#app");
