b0ef753bf0e2c1b1345552d41f6e49aa761be565
hbollon <hugo.bollon@gmail.com>
2022-02-13 18:33:50 +0100

    feat: update app for header and footer components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,46 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <Header/>
   <router-view/>
+  <Footer/>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Header from './components/sections/Header.vue';
+import Footer from './components/sections/Footer.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Header,    
+    Footer,    
+  },
+});
+</script>
+
+<style lang="scss">
+#app {
+  width: 100%;
+  max-width: 100vw;
+  overflow-wrap: break-word;
+  overflow-x: hidden;
+}
+
+.route-enter-from {
+  opacity: 0;
+  transform: translateY(100px);
+}
+
+.route-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.route-leave-to {
+  opacity: 0;
+  transform: translateY(-100px);
+}
+
+.route-leave-active {
+  transition: all 0.3s ease-in;
+}
+</style>
